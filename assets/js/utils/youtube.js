f50e36bbb0cfb77b5b063b17667432a70d86835b
Nicolas Carbone <nico.sambora@gmail.com>
2015-08-19 01:03:47 +0000

    wip getting categories

--- assets/js/utils/youtube.js
+++ assets/js/utils/youtube.js
@@ -1,119 +1,119 @@
 
-var $											= require('jquery'),
-		_											= require('underscore'),
-		Backbone 							= require('backbone'),
-		React 								= require('react'),
-		KeysModule 						= require('../utils/keys.js'),
-    YTVideosComponent 		= require('../components/youtube.component.js'),
-    YTCategoriesComponent	= require('../components/youtube.categories.component.js');
+var $                     = require('jquery'),
+    _                     = require('underscore'),
+    Backbone              = require('backbone'),
+    React                 = require('react'),
+    KeysModule            = require('../utils/keys.js'),
+    YTVideosComponent     = require('../components/youtube.component.js'),
+    YTCategoriesComponent = require('../components/youtube.categories.component.js');
 
 var YoutubeView = Backbone.View.extend({
 
-	gapi: null,
+  gapi: null,
 
-	initialize: function() {
+  initialize: function() {
 
-		var self = this,
-    		keys = KeysModule();
+    var self = this,
+        keys = KeysModule();
 
-		this.OAUTH2_CLIENT_ID = keys.OAUTH2_CLIENT_ID
+    this.OAUTH2_CLIENT_ID = keys.OAUTH2_CLIENT_ID
 
-		this.OAUTH2_SCOPES = [
-		  'https://www.googleapis.com/auth/youtube'
-		];
+    this.OAUTH2_SCOPES = [
+      'https://www.googleapis.com/auth/youtube'
+    ];
 
-		window.gapi_onload = function() {
-			console.log("gapi_onload");
-			self.googleApiClientReady();
-		}
+    window.gapi_onload = function() {
+      console.log("gapi_onload");
+      self.googleApiClientReady();
+    }
 
-		$.getScript('https://apis.google.com/js/client.js');
+    $.getScript('https://apis.google.com/js/client.js');
 
-	},
+  },
 
 
-	googleApiClientReady: function() {
-		var self = this;
-	  gapi.auth.init(function() {
-	  	self.checkAuth();
-	  });
-	},
+  googleApiClientReady: function() {
+    var self = this;
+    gapi.auth.init(function() {
+      self.checkAuth();
+    });
+  },
 
-	checkAuth: function() {
+  checkAuth: function() {
 
-		var self = this;
-	  gapi.auth.authorize({
-	    client_id: self.OAUTH2_CLIENT_ID,
-	    scope: self.OAUTH2_SCOPES,
-	    immediate: true
-	  }, function( authResult ) {
-	  	if (authResult && !authResult.error) {
-	  		self.loadAPIClientInterfaces();
-	  	}
-	  });
+    var self = this;
+    gapi.auth.authorize({
+      client_id: self.OAUTH2_CLIENT_ID,
+      scope: self.OAUTH2_SCOPES,
+      immediate: true
+    }, function( authResult ) {
+      if (authResult && !authResult.error) {
+        self.loadAPIClientInterfaces();
+      }
+    });
 
-	},
+  },
 
-	handleAPILoaded: function() {
-	  $('#spinner-loader').hide();
-	},
+  handleAPILoaded: function() {
+    $('#spinner-loader').hide();
+  },
 
-	loadAPIClientInterfaces: function() {
-		var self = this;
-	  gapi.client.load('youtube', 'v3', function() {
-	    self.handleAPILoaded();
-	  });
-	},
+  loadAPIClientInterfaces: function() {
+    var self = this;
+    gapi.client.load('youtube', 'v3', function() {
+      self.handleAPILoaded();
+    });
+  },
 
-	render: function( videos ) {
+  render: function( videos ) {
 
-		var ids = [];
+    var ids = [];
 
-		_.each(videos, function( video, index ) {
-	  	ids.push( video.id.videoId );
-	  });
+    _.each(videos, function( video, index ) {
+      ids.push( video.id.videoId );
+    });
 
-	  var request = gapi.client.youtube.videos.list({
-		  id: ids.join(),
-		  part: 'snippet'
-		});
+    var request = gapi.client.youtube.videos.list({
+      id: ids.join(),
+      part: 'snippet'
+    });
 
-	  request.execute(function( response ) {
+    request.execute(function( response ) {
 
-	  	var categories = [];
-	  	_.each(response.items, function( video, index ) {
-	  		if ( $.inArray(video.snippet.categoryId, categories) === -1 ) {
-	  			categories.push( video.snippet.categoryId );
-	  		}
-		  });
+      var categories = [];
+      _.each(response.items, function( video, index ) {
+        if ( $.inArray(video.snippet.categoryId, categories) === -1 ) {
+          categories.push( video.snippet.categoryId );
+        }
+      });
 
-		  var request = gapi.client.youtube.videoCategories.list({
-			  id: categories.join(),
-			  part: 'snippet'
-			});
+      var request = gapi.client.youtube.videoCategories.list({
+        id: categories.join(),
+        part: 'snippet'
+      });
 
-	  	request.execute(function( response ) {
-	  		React.render(<YTCategoriesComponent categories={response.items} videos={videos} />, $('#videos').get(0));
-	  		//React.render(<YTVideosComponent videos={videos} />, $('#videos').get(0));
-	  	});
+      request.execute(function( response ) {
+        React.render(<YTCategoriesComponent categories={response.items} videos={videos} />, $('#videos').get(0));
+        //React.render(<YTVideosComponent videos={videos} />, $('#videos').get(0));
+      });
 
-	  });
+    });
 
-	},
+  },
 
-	// Search for a specified string.
-	search: function( q ) {
-	  var self = this,
-	  		request = gapi.client.youtube.search.list({
-			    q: q,
-			    part: 'snippet'
-			  });
+  // Search for a specified string.
+  search: function( q ) {
+    var self = this,
+        request = gapi.client.youtube.search.list({
+          q: q,
+          part: 'snippet'
+        });
 
-	  request.execute(function( response ) {
-	  	self.render( response.items );
-	  });
+    request.execute(function( response ) {
+      self.render( response.items );
+    });
 
-	}
+  }
 
 });
 
